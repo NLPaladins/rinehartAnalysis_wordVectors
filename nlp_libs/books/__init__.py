11204bd92f217f9adef46bced598042d3234f766
drkostas <georgiou.kostas94@gmail.com>
2021-10-18 23:18:32 -0400

    Created basic structure of the notebook #1

---
+++ nlp_libs/books/__init__.py
@@ -0,0 +1,10 @@
+"""Books sub-package."""
+
+from .preprocessing import *
+from .word_embeddings import *
+from .compare_statistics import *
+from .plotter import *
+
+__email__ = "jmerlet@vols.utk.edu, kgeorgio.vols.utk.edu, mlane42@vols.utk.edu"
+__author__ = "jeanmerlet, drkostas, LaneMatthewJ"
+__version__ = "0.1.0"
---
+++ nlp_libs/books/plotter.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+import matplotlib.colors as plt_colors
+import matplotlib.lines as mlines
+import numpy as np
+
