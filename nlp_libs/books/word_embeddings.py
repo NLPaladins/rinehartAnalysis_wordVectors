d76b79635c042ca30081623e89556ae2316e4f07
drkostas <georgiou.kostas94@gmail.com>
2021-10-31 23:11:30 -0400

    Pretrained done #4

--- nlp_libs/books/word_embeddings.py
+++ nlp_libs/books/word_embeddings.py
@@ -1,9 +1,88 @@
-from gensim.test.utils import common_texts
 import gensim.models
+import gensim.downloader
 import numpy as np
 import pandas as pd
 import itertools
 from typing import *
+
+
+def calculate_differing_distances(word_pairs: List[Tuple[str]],
+                                  sentences: List[str] = None,
+                                  model_names: List[str] = None):
+    skipped_words = []
+    vector_dimensions = [50, 100, 200, 300]
+    window_dimensions = [2, 5, 3, 10]
+
+    if model_names is not None:
+        df = pd.DataFrame(
+            columns=['word1', 'word2', 'model', 'cosineSim', 'dotSim'])
+        for model_name in model_names:
+            wv = get_model_wv(model_name)
+            df = get_word_pair_distances(df=df, wv=wv, word_pairs=word_pairs,
+                                         model_name=model_name, skipped_words=skipped_words)
+    elif sentences is not None:
+        df = pd.DataFrame(
+            columns=['word1', 'word2', 'vectorSize', 'windowSize', 'cosineSim', 'dotSim'])
+        for vector_size in vector_dimensions:
+            for window_size in window_dimensions:
+                model = gensim.models.Word2Vec(
+                    sentences, vector_size=vector_size, window=window_size, min_count=2)
+                df = get_word_pair_distances(df=df, wv=model.wv, word_pairs=word_pairs,
+                                             vector_size=vector_size, window_size=window_size,
+                                             skipped_words=skipped_words)
+    else:
+        raise Exception("You should either set `model` or `sentences`.")
+
+    return df
+
+
+def get_word_pair_distances(df: pd.DataFrame,
+                            word_pairs: List[Tuple[str]],
+                            wv: gensim.models.keyedvectors.KeyedVectors,
+                            skipped_words: List[str],
+                            vector_size: int = None,
+                            window_size: int = None,
+                            model_name: str = None) -> pd.DataFrame:
+    for wordPair in word_pairs:
+        cos_similarity = get_distance(wordPair[0], wordPair[1],
+                                      wv, skipped_words, 'cosine')
+        dot_similarity = get_distance(wordPair[0], wordPair[1],
+                                      wv, skipped_words, 'dot')
+        if cos_similarity is None or dot_similarity is None:
+            continue
+        if vector_size is not None:
+            data = {
+                "word1": wordPair[0],
+                "word2": wordPair[1],
+                "vectorSize": vector_size,
+                "windowSize": window_size,
+                "cosineSim": cos_similarity,
+                "dotSim": dot_similarity,
+            }
+        elif model_name is not None:
+            data = {
+                "word1": wordPair[0],
+                "word2": wordPair[1],
+                "model_name": model_name,
+                "cosineSim": cos_similarity,
+                "dotSim": dot_similarity,
+            }
+        else:
+            raise Exception("You should either set `model_name` or (`vector_size` and `window_size`)")
+
+        df = df.append(data, ignore_index=True)
+
+        return df
+
+
+def get_model_names() -> List[str]:
+    return list(gensim.downloader.info()['models'].keys())
+
+
+def get_model_wv(name: str) -> gensim.models.keyedvectors.KeyedVectors:
+    wv = gensim.downloader.load(name)
+
+    return wv
 
 
 def get_distance(word1, word2, wv, skipped_words: List[str], dist_type='cosine'):
@@ -21,41 +100,6 @@ def get_distance(word1, word2, wv, skipped_words: List[str], dist_type='cosine')
         distance = wv.similarity(word1, word2) if dist_type == 'cosine' \
             else np.dot(wv[word1], wv[word2])
         return distance
-
-
-def calculate_differing_distances(wordpairs, sentences):
-    """ If sentences is None, use pretrained"""
-    if sentences is None:
-        sentences = common_texts
-    skipped_words = []
-    vector_dimensions = [50, 100, 200, 300]
-    window_dimensions = [2, 5, 3, 10]
-
-    df = pd.DataFrame(columns=['word1', 'word2', 'vectorSize', 'windowSize', 'cosineSim', 'dotSim'])
-
-    for vector_size in vector_dimensions:
-        for window_size in window_dimensions:
-            model = gensim.models.Word2Vec(
-                sentences, vector_size=vector_size, window=window_size, min_count=2)
-
-            for wordPair in wordpairs:
-                cosSimilarity = get_distance(wordPair[0], wordPair[1],
-                                             model.wv, skipped_words, 'cosine')
-                dotSimilarity = get_distance(wordPair[0], wordPair[1],
-                                             model.wv, skipped_words, 'dot')
-                if cosSimilarity is None or dotSimilarity is None:
-                    continue
-                data = {
-                    "word1": wordPair[0],
-                    "word2": wordPair[1],
-                    "vectorSize": vector_size,
-                    "windowSize": window_size,
-                    "cosineSim": cosSimilarity,
-                    "dotSim": dotSimilarity,
-                }
-                df = df.append(data, ignore_index=True)
-
-    return df
 
 
 def get_conf_values(conf: Dict, keys: List[str], get_all_sub_values: bool,
